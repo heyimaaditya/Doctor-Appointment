00161f1e198246a67303ef9589e5749352a1d302
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-18 17:02:33 +0530

    frontend part start

---
+++ frontend/src/app/features/user/apply-doctor/apply-doctor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-doctor',
+  imports: [],
+  templateUrl: './apply-doctor.component.html',
+  styleUrl: './apply-doctor.component.scss'
+})
+export class ApplyDoctorComponent {
+
+}
