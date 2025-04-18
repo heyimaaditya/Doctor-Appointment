6c92cba00eccf9459364218836927148ae6f4685
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-18 09:43:56 +0530

    database connection using sequelize

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,15 @@
+import {Sequelize} from "sequelize";
+import * as dotenv from "dotenv";
+//import sequelize from 'sequelize';
+dotenv.config();
+const sequelize = new Sequelize(
+    process.env.DB_NAME || "doctor_appointment",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASSWORD || "postgres",
+    {
+        host: process.env.DB_HOST || "db",
+        dialect: "postgres",
+        port: parseInt(process.env.DB_PORT || "5432"),
+    }
+)
+export default sequelize;
