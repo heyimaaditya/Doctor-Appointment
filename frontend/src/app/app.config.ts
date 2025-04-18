ef4002bf2200f252ca0b51fa63e4084dd7b71f1c
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-18 17:07:45 +0530

    configure ng-zorro-antd and HTTPClient

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,10 +7,24 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
-
+import { provideHttpClient,withInterceptors } from '@angular/common/http';
+import { NzConfig,provideNzConfig } from 'ng-zorro-antd/core/config';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 registerLocaleData(en);
 
+const ngZorroConfig: NzConfig = {
+  message: { nzMaxStack: 3, nzDuration: 3000 }, // Example global config
+  notification: { nzMaxStack: 3 }
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), // Default
+    provideRouter(routes), // Provide main routes
+    provideHttpClient(withInterceptors([authInterceptor])), // Provide HttpClient with interceptor
+    provideAnimationsAsync(), // For NgZorro animations
+    provideNzI18n(en_US), // NgZorro internationalization
+    provideNzConfig(ngZorroConfig) // Optional global NgZorro config
+    // Add other global services if not using `providedIn: 'root'`
+  ]
 };
